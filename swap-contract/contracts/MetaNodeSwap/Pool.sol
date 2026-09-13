0cd7a0487ca89ab82d7ac65876a55feff5ca257a
nil2000 <2628636987@qq.com>
2026-09-13 15:59:04 +0800

    添加Pool.sol合约 注释

--- swap-contract/contracts/MetaNodeSwap/Pool.sol
+++ swap-contract/contracts/MetaNodeSwap/Pool.sol
@@ -2,65 +2,99 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// ERC20 标准接口：查询余额、转账等
 
 import "./libraries/SqrtPriceMath.sol";
+// 根据价格和流动性计算 Token 数量
+
 import "./libraries/TickMath.sol";
+// Tick ↔ sqrtPrice 相互转换
+
 import "./libraries/LiquidityMath.sol";
+// 流动性加减计算
+
 import "./libraries/LowGasSafeMath.sol";
+// 安全的加减法
+
 import "./libraries/SafeCast.sol";
+// 不同整数类型之间安全转换
+
 import "./libraries/TransferHelper.sol";
+// 安全转 Token
+
 import "./libraries/SwapMath.sol";
+// 计算 Swap 每一步的结果
+
 import "./libraries/FixedPoint128.sol";
+// Q128 定点数常量
 
 import "./interfaces/IPool.sol";
+// Pool 对外接口
+
 import "./interfaces/IFactory.sol";
+// Factory 接口
+
 
 contract Pool is IPool {
+    // Pool 合约，实现 IPool 接口
+
     using SafeCast for uint256;
+    // uint256 可以直接使用 SafeCast 的转换方法
+
     using LowGasSafeMath for int256;
+    // int256 使用安全数学运算
+
     using LowGasSafeMath for uint256;
+    // uint256 使用安全数学运算
 
-    /// @inheritdoc IPool
+    // @inheritdoc IPool   创建这个 Pool 的 Factory 地址
     address public immutable override factory;
-    /// @inheritdoc IPool
+    // @inheritdoc IPool    交易对中的 Token0
     address public immutable override token0;
-    /// @inheritdoc IPool
+    //  @inheritdoc IPool   交易对中的 Token1
     address public immutable override token1;
-    /// @inheritdoc IPool
+    //  @inheritdoc IPool   手续费，例如 3000 = 0.3%
     uint24 public immutable override fee;
-    /// @inheritdoc IPool
+    //  @inheritdoc IPool  这个 Pool 的最低 Tick
     int24 public immutable override tickLower;
-    /// @inheritdoc IPool
+    //  @inheritdoc IPool  这个 Pool 的最高 Tick  
     int24 public immutable override tickUpper;
-
-    /// @inheritdoc IPool
+    // immutable 可以简单理解为：
+    // 部署时确定，之后不能修改。
+    // @inheritdoc IPool   当前价格的 sqrtPrice，使用 Q96 定点数
+    // 保存当前价格的平方根，但为了让 Solidity 能精确计算，把它乘以 2^96 后作为整数保存
     uint160 public override sqrtPriceX96;
-    /// @inheritdoc IPool
+    // @inheritdoc IPool  价格所在的“刻度/位置”  它不是价格本身，而是用一个整数来表示价格位置
     int24 public override tick;
-    /// @inheritdoc IPool
+    //  @inheritdoc IPool
     uint128 public override liquidity;
 
-    /// @inheritdoc IPool
+    // @inheritdoc IPool  全池 token0 手续费增长“里程表”
     uint256 public override feeGrowthGlobal0X128;
-    /// @inheritdoc IPool
+    // @inheritdoc IPool  全池 token1 手续费增长“里程表”
     uint256 public override feeGrowthGlobal1X128;
-
+    // 记录一个 LP 在这个 Pool 里的“账户信息”
     struct Position {
-        // 该 Position 拥有的流动性
+        // 该 Position 拥有的流动性 ； 这个 LP 当前在这个 Pool 中提供了多少流动性
         uint128 liquidity;
-        // 可提取的 token0 数量
+        // 可提取的 token0 数量  这个 LP 目前已经累计、可以领取的 Token0。记录在这里 ≠ 钱已经转到 LP 钱包。
+         //tokensOwed0 → 欠 LP 的 Token0
+        // tokensOwed1 → 欠 LP 的 Token1
         uint128 tokensOwed0;
         // 可提取的 token1 数量
         uint128 tokensOwed1;
-        // 上次提取手续费时的 feeGrowthGlobal0X128
+        // 这个 Position 上一次结算手续费时，Token0 的手续费累计值是多少。
+        // 上次结算时的里程表读数
         uint256 feeGrowthInside0LastX128;
-        // 上次提取手续费是的 feeGrowthGlobal1X128
+      
         uint256 feeGrowthInside1LastX128;
     }
 
     // 用一个 mapping 来存放所有 Position 的信息
+    // 通过用户的钱包地址，找到这个用户在当前 Pool 里的 Position（仓位）信息
+    // 钱包地址  →  Position仓位
     mapping(address => Position) public positions;
-
+    // 直接访问仓位数据
     function getPosition(
         address owner
     )
@@ -83,7 +117,7 @@ contract Pool is IPool {
             positions[owner].tokensOwed1
         );
     }
-
+// Pool 被 Factory 创建时，通过 msg.sender 找到 Factory，再从 Factory 的 parameters() 中把自己需要的初始化参数取出来
     constructor() {
         // constructor 中初始化 immutable 的常量
         // Factory 创建 Pool 时会通 new Pool{salt: salt}() 的方式创建 Pool 合约，通过 salt 指定 Pool 的地址，这样其他地方也可以推算出 Pool 的地址
@@ -94,6 +128,11 @@ contract Pool is IPool {
         ).parameters();
     }
 
+
+    // sqrtPriceX96 = 当前具体价格的数学表示
+    // tick = 当前价格所在的离散价格刻度
+    // getTickAtSqrtPrice() = 根据价格反查 tick。
+
     function initialize(uint160 sqrtPriceX96_) external override {
         require(sqrtPriceX96 == 0, "INITIALIZED");
         // 通过价格获取 tick，判断 tick 是否在 tickLower 和 tickUpper 之间
@@ -103,36 +142,48 @@ contract Pool is IPool {
             "sqrtPriceX96 should be within the range of [tickLower, tickUpper)"
         );
         // 初始化 Pool 的 sqrtPriceX96
+        // 保存当前价格的平方根，但为了让 Solidity 能精确计算，把它乘以 2^96 后作为整数保存
         sqrtPriceX96 = sqrtPriceX96_;
     }
 
+    // 修改一个 LP 仓位时，需要带进去的两个参数
     struct ModifyPositionParams {
-        // the address that owns the position
+        // the address that owns the position    // 持有这个仓位的人
         address owner;
-        // any change in liquidity
+        // any change in liquidity   // 流动性要增加还是减少
         int128 liquidityDelta;
     }
 
+   // 修改 LP 仓位的总入口：先算这次需要多少 token，再结算旧手续费，最后更新流动性。
     function _modifyPosition(
         ModifyPositionParams memory params
     ) private returns (int256 amount0, int256 amount1) {
-        // 通过新增的流动性计算 amount0 和 amount1
+        //1. 通过新增的流动性计算 amount0 和 amount1
         // 参考 UniswapV3 的代码
+
+        // 根据增加/减少的流动性，计算需要多少 token0 
+        // 可以理解为：这次改仓，要动多少 token0
 
         amount0 = SqrtPriceMath.getAmount0Delta(
             sqrtPriceX96,
             TickMath.getSqrtPriceAtTick(tickUpper),
             params.liquidityDelta
         );
-
+        // 根据增加/减少的流动性，计算需要多少 token1 
+        // 可以理解为：这次改仓，要动多少 token1
         amount1 = SqrtPriceMath.getAmount1Delta(
             TickMath.getSqrtPriceAtTick(tickLower),
             sqrtPriceX96,
             params.liquidityDelta
         );
+
+        //2. 找到这个 LP 自己的仓位账本
         Position storage position = positions[params.owner];
 
         // 提取手续费，计算从上一次提取到当前的手续费
+        //3. 计算这段时间新产生的 token0 手续费 
+        // 当前手续费累计值 - 上次记录的值
+        // 再 × LP 的流动性 = 这次新增的手续费
         uint128 tokensOwed0 = uint128(
             FullMath.mulDiv(
                 feeGrowthGlobal0X128 - position.feeGrowthInside0LastX128,
@@ -140,6 +191,7 @@ contract Pool is IPool {
                 FixedPoint128.Q128
             )
         );
+        // 3.1同理，计算新产生的 token1 手续费
         uint128 tokensOwed1 = uint128(
             FullMath.mulDiv(
                 feeGrowthGlobal1X128 - position.feeGrowthInside1LastX128,
@@ -149,17 +201,23 @@ contract Pool is IPool {
         );
 
         // 更新提取手续费的记录，同步到当前最新的 feeGrowthGlobal0X128，代表都提取完了
+        // 更新手续费“里程表” // 记录已经结算到当前这个位置
         position.feeGrowthInside0LastX128 = feeGrowthGlobal0X128;
         position.feeGrowthInside1LastX128 = feeGrowthGlobal1X128;
         // 把可以提取的手续费记录到 tokensOwed0 和 tokensOwed1 中
         // LP 可以通过 collect 来最终提取到用户自己账户上
+        // 把刚刚算出来的手续费记到账本里 
+        // 注意：这里只是记账，还没有真正转 token
+         // 真正把钱转给 LP，要等 collect()
         if (tokensOwed0 > 0 || tokensOwed1 > 0) {
             position.tokensOwed0 += tokensOwed0;
             position.tokensOwed1 += tokensOwed1;
         }
 
-        // 修改 liquidity
+        // 修改 liquidity        // 修改整个 Pool 的流动性 // liquidityDelta > 0：增加 // liquidityDelta < 0：减少
         liquidity = LiquidityMath.addDelta(liquidity, params.liquidityDelta);
+
+        // 修改这个 LP 自己的流动性
         position.liquidity = LiquidityMath.addDelta(
             position.liquidity,
             params.liquidityDelta
@@ -429,6 +487,21 @@ contract Pool is IPool {
                     uint256(-amount0)
                 );
         }
+
+
+        //         谁交易
+        // ↓
+        // 谁收 Token
+        // ↓
+        // Token0 变化多少
+        // ↓
+        // Token1 变化多少
+        // ↓
+        // 交易后价格
+        // ↓
+        // 交易后流动性
+        // ↓
+        // 交易后 Tick
 
         emit Swap(
             msg.sender,
